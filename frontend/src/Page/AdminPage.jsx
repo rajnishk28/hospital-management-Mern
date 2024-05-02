e8de016ee175745578563e8cc4e70c724d7495eb
rajnish <rajnisharyan12@gmail.com>
2024-05-02 09:19:41 +0530

    sidebar bug fixed

--- frontend/src/Page/AdminPage.jsx
+++ frontend/src/Page/AdminPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PatientDetails from "../Components/Admin/PatientDetails/PatinetDetails"
-import Sidebar from '../Components/Admin/Sidebar/Sidebar';
+import Sidebar from "../Components/Admin/SideBar/Sidebar"
 import Home from '../Components/Admin/Home/Home';
 import NotFound from '../Components/Admin/NotFound/NotFound';
 import AdminProfile from "../Components/Admin/Profile/AdminProfile"
